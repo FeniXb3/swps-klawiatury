5babbb65158ee2c4fee9b7b35e27b352ff2fde8f
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-03-16 17:59:16 +0100

    Moving around

---
+++ Klawiatury/Player.cs
@@ -0,0 +1,33 @@
+class Player
+{
+    public string name;
+    public int x;
+    public int y;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+}
+
+/*
+Player:
+- dane:
+    - name
+    - hp
+    - maxhp
+    - stamina
+    - maxstamina
+    - attackStrength
+    - currentWeapon
+    - inventory
+    - position
+    - speed
+- akcje:
+    - takeDamage
+    - heal
+    - levelUp
+    - attack
+    - die
+    - move
+*/
