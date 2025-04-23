ad9eeda4f9cfd6a386e3a607fb5c0ba7ca18d86e
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-23 08:55:03 +0200

    Extract moving to method in Player class

--- Klawiatury/Program.cs
+++ Klawiatury/Program.cs
@@ -54,42 +54,7 @@ while (true)
 
         foreach (Player element in clones)
         {
-            int targetX = element.position.x;
-            int targetY = element.position.y;
-
-            int signY = Math.Sign(direction.y);
-            int signX = Math.Sign(direction.x);
-
-            for (int i = 1; i <= Math.Abs(direction.y * element.speed); i++)
-            {
-                int coordinateToTest = element.position.y + i * signY;
-                if (coordinateToTest >= level.Length || coordinateToTest < 0|| level[coordinateToTest][targetX] == '#')
-                {
-                    break;
-                }
-                else
-                {
-                    targetY = coordinateToTest;
-                }
-            }
-
-            for (int i = 1; i <= Math.Abs(direction.x * element.speed); i++)
-            {
-                int coordinateToTest = element.position.x + i * signX;
-                if (coordinateToTest >= level[targetY].Length || coordinateToTest < 0 || level[targetY][coordinateToTest] == '#')
-                {
-                    break;
-                }
-                else
-                {
-                    targetX = coordinateToTest;
-                }
-            }
-            
-            element.position.x = targetX;
-            element.position.y = targetY;
-            
-            element.speed += 1;
+            element.Move(direction, level);
         }
     }
     else
