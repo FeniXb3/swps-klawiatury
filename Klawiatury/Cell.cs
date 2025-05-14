6987252d4a1ac64c164c6d1b95ffd3b3942d96a1
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-05-14 13:41:10 +0200

    Change level data type to array of arrays of Cells

---
+++ Klawiatury/Cell.cs
@@ -0,0 +1,10 @@
+class Cell
+{
+    public char visual;
+    public Character? occupant;
+
+    public Cell(char visual)
+    {
+        this.visual = visual;
+    }
+}
--- Klawiatury/Level.cs
+++ Klawiatury/Level.cs
@@ -1,6 +1,6 @@
 class Level
 {
-    private string[] levelData =
+    private string[] levelVisuals =
     [
         "#.############################################",
         "..............................................",
@@ -14,19 +14,40 @@ class Level
         "#########################################",
     ];
 
+    private Cell[][] levelData;
+
+    public Level()
+    {
+        levelData = new Cell[levelVisuals.Length][];
+        for (int y = 0; y < levelVisuals.Length; y++)
+        {
+            string row = levelVisuals[y];
+            Cell[] dataRow = new Cell[row.Length];
+            for (int i = 0; i < row.Length; i++)
+            {
+                dataRow[i] = new Cell(row[i]);
+            }
+            levelData[y] = dataRow;
+        }
+    }
+
     public void Display()
     {
-        foreach (string row in levelData)
+        foreach (Cell[] row in levelData)
         {
-            Console.WriteLine(row);
+            foreach (Cell cell in row)
+            {
+                Console.Write(cell.visual);
+            }
+            Console.WriteLine();
         }
     }
 
     public void RedrawCell(Point position)
     {
         Console.SetCursorPosition(position.x, position.y);
-        string row = levelData[position.y];
-        char cellData = row[position.x];
+        Cell[] row = levelData[position.y];
+        char cellData = row[position.x].visual;
         Console.Write(cellData);
     }
 
@@ -37,7 +58,7 @@ class Level
 
     public char GetCell(int x, int y)
     {
-        return levelData[y][x];
+        return levelData[y][x].visual;
     }
 
     public int GetRowWidth(int rowIndex)
